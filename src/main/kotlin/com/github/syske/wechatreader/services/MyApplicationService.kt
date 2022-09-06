056d813fd64b93dad5d1433772d4f24bb742c38a
GitHub Action <action@github.com>
2022-09-06 07:45:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/syske/wechatreader/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/syske/wechatreader/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.syske.wechatreader.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.syske.wechatreader.MyBundle
 
 class MyApplicationService {
 
